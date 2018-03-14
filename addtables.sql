b8689da920edc79688d524b1d4929abdf31812e3
Veer Muchandi <veer.muchandi@gmail.com>
2018-03-13 23:04:21 -0400

    added addtables script for mssql

---
+++ addtables.sql
@@ -0,0 +1,7 @@
+USE sampledb 
+
+CREATE TABLE users (user_id INT, username NVARCHAR(50))
+
+INSERT INTO users VALUES (1, 'alice');
+INSERT INTO users VALUES (2, 'bob');
+INSERT INTO users VALUES (3, 'charlie');
